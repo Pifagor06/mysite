065c8cca0477bada1f231636ee6f1ac5576aaef3
Pifagor06 <farhatshohratov@gmail.com>
2025-01-13 10:27:27 +0500

    Initial commit

---
+++ testapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Rubric
+
+# Create your views here.
+def test(request):
+    return render(request, "testapp/test.html", {'rubrics': Rubric.objects.all()})
+
+def get_rubric(request):
+    pass
